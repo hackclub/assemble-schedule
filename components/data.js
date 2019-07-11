b64f2f6103ef1be1bef6713fc8e97b463090699c
Glitch (flagship-schedule) <none>
2019-07-11 23:42:28 +0000

    💄🌍 Checkpoint ./components/data.js:460837/47

--- components/data.js
+++ components/data.js
@@ -8,7 +8,7 @@ const schedule = [
     },
     events: [
       { name: 'Registration', subtitle: 'Papers, please', start: '12:00 pm', end: '2:00 pm' },
-      { name: 'Opening ceremony', subtitle: 'Keynote: Jeremy Rossmann, co-founder of Make School', start: '2:00 pm', end: '3:15 pm', summary: 'Planned for 45 minutes, followed by a break until the next event' },
+      { name: 'Opening ceremony', subtitle: 'Keynote: Jeremy Rossmann,\nco-founder of Make School', start: '2:00 pm', end: '3:15 pm', summary: 'Planned for 45 minutes, followed by a break until the next event' },
       { name: 'Make School Workshop', start: '3:15 pm', end: '4:30 pm' },
       { name: 'Industry Keynote: Kat Mañalac', start: '4:30 pm', end: '5:00 pm' },
       { name: 'Dinner', start: '5:00 pm', end: '10:00 pm', flavor: 'food' },
@@ -24,7 +24,7 @@ const schedule = [
     events: [
       { name: 'Check-in / Breakfast', subtitle: 'Fresh dino eggs', start: '9:00 am', end: '9:30 am', flavor: 'food' },
       { name: 'Founder Keynote: Zach Latta', start: '9:30 am', end: '10:30 am' },
-      { name: 'Break-out sessions', subtitle: '1st Hackathon | Hacker Tools | ', start: '10:30 am', end: '11:30 am' },
+      { name: 'Break-out sessions', subtitle: '1st Hackathon | Hacker Tools | Running club meetings beyond the workshops', start: '10:30 am', end: '11:30 am' },
       { name: 'Break-out sessions', start: '11:30 am', end: '12:30 pm' },
       { name: 'Lunch', subtitle: 'Get your food!', start: '12:30 pm', end: '1:45 pm', flavor: 'food' },
       { name: 'Break-out sessions', start: '1:45 pm', end: '2:30 pm' },
