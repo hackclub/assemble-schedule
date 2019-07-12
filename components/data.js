9dd8b11dce74470112f9596597029906d3c4f56a
Glitch (flagship-schedule) <none>
2019-07-12 15:42:47 +0000

    🌜🍚 Checkpoint ./components/data.js:460837/7253

--- components/data.js
+++ components/data.js
@@ -15,11 +15,11 @@ const schedule = [
     },
     events: [
       { name: 'Registration', start: '12:00 pm', end: '2:00 pm', summary: 'For questions or assistance: (415) 805-1774' },
-      { name: 'Opening ceremony', start: '2:00 pm', end: '3:15 pm' },
-      { name: 'Keynote: Jeremy Rossmann', subtitle: 'Co-founder of Make School', start: '9:30 am', end: '10:30 am',  },
+      { name: 'Opening ceremony', start: '2:00 pm', end: '2:30 pm' },
+      { name: 'Keynote: Jeremy Rossmann', subtitle: 'Co-founder of Make School', start: '2:30 pm', end: '3:15 pm',  },
       { name: 'Pitch Your Passion', subtitle: 'Make School Workshop', start: '3:15 pm', end: '4:30 pm', summary: 'How to pitch your passion for programming to inspire other and grow your club and hackathon attendance. Featuring Jordan Arnesen, Curriculum Engineer at Make School.' },
       { name: 'Keynote: Kat Mañalac', subtitle: 'YC Partner', start: '4:30 pm', end: '5:00 pm', summary: 'Kat was Chief of Staff to Alexis Ohanian, cofounder of reddit, before joining YC as its Director of Outreach.' },
-      { name: 'Dinner', start: '5:00 pm', end: '6:00 pm', flavor: 'food' },
+      { name: 'Dinner', start: '5:00 pm', end: '6:00 pm', flavor: 'food', summary: 'Pizza by the slice. Alternative meal options available upon request.' },
       { name: 'Afternoon activities', start: '6:00 pm', end: '10:00 pm', summary: 'Check-in at hostels or hang out at the venue' },
     ]
   },
@@ -35,7 +35,7 @@ const schedule = [
       { name: 'Keynote: Zach Latta', subtitle: 'Founder of Hack Club', start: '9:30 am', end: '10:30 am' },
       { name: 'Break-out sessions', start: '10:30 am', end: '11:30 am' },
       { name: 'Break-out sessions', start: '11:30 am', end: '12:30 pm' },
-      { name: 'Lunch', subtitle: 'Get your food!', start: '12:30 pm', end: '1:45 pm', flavor: 'food' },
+      { name: 'Lunch', subtitle: 'Get your food!', start: '12:30 pm', end: '1:45 pm', flavor: 'food', summary: '' },
       { name: 'Break-out sessions', start: '1:45 pm', end: '2:30 pm' },
       { name: 'Office hours', start: '2:30 pm', end: '3:45 pm', summary: `
 Guests with available office hours:
@@ -53,7 +53,7 @@ You can request office hours with a random guest by reaching out to hello@flagsh
       { name: 'Closing remarks', start: '4:30 pm', end: '5:00 pm' },
       { name: 'Cybersecurity', start: '5:00 pm', end: '6:15 pm' },
       { name: 'Dinner', start: '6:15 pm', end: '8:15 pm', flavor: 'food' },
-      { name: 'Unstructured / Games', start: '8:15 pm', end: '10:00 pm' },
+      { name: 'Unstructured Games', start: '8:15 pm', end: '10:00 pm' },
     ]
   },
   {
