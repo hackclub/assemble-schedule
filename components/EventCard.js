3ca0e6ee61f3479a92499b294d4fff0e2c8c6aec
Pranav Teegavarapu <pranavnt@outlook.com>
2022-08-05 16:37:17 -0700

    LFG

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react'
-import styled, { css } from 'styled-components'
-import { Heading, Box, Text, Icon } from '@hackclub/design-system'
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import { Heading, Box, Text, Icon } from "@hackclub/design-system";
 // import {MDXProvider} from '@mdx-js/react'
 // import MDX from '@mdx-js/runtime'
-import MD from 'react-markdown'
-import theme from './style'
-import Modal from './Modal'
+import MD from "react-markdown";
+import theme from "./style";
+import Modal from "./Modal";
 
 // Pasted straight from https://css-tricks.com/snippets/javascript/lighten-darken-color/
 function LightenDarkenColor(col, amt) {
-  let usePound = false
-  if (col[0] == '#') {
-    col = col.slice(1)
-    usePound = true
+  let usePound = false;
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
   }
-  const num = parseInt(col, 16)
-  const r = (num >> 16) + amt
-  if (r > 255) r = 255
-  else if (r < 0) r = 0
-  const b = ((num >> 8) & 0x00ff) + amt
-  if (b > 255) b = 255
-  else if (b < 0) b = 0
-  const g = (num & 0x0000ff) + amt
-  if (g > 255) g = 255
-  else if (g < 0) g = 0
-  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16)
+  const num = parseInt(col, 16);
+  const r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+  const b = ((num >> 8) & 0x00ff) + amt;
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+  const g = (num & 0x0000ff) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
 }
 
 const Block = styled(Box)`
@@ -33,65 +33,68 @@ const Block = styled(Box)`
   position: absolute;
   left: 4em;
   right: 1em;
-  top: ${props => theme.space[5] * (toSpecialTime(props.start) + 0.5)}px;
-  height: ${props =>
+  top: ${(props) => theme.space[5] * (toSpecialTime(props.start) + 0.5)}px;
+  height: ${(props) =>
     theme.space[5] * (toSpecialTime(props.end) - toSpecialTime(props.start)) -
     1}px;
   z-index: 2;
-  ${props =>
-    props.flavor == 'closing' && `background-color: ${theme.colors.red} !important;`}
-  ${props =>
-    props.flavor == 'food' &&
+  ${(props) =>
+    props.flavor == "closing" &&
+    `background-color: ${theme.colors.red} !important;`}
+  ${(props) =>
+    props.flavor == "food" &&
     `background-color: ${theme.colors.orange} !important;`}
   
-  ${props => (props.onClick === null) ? null :
-  css`
-    cursor: pointer;
-    transition-duration: 0.25s;
-    &:hover {
-      // background: ${LightenDarkenColor(theme.colors.blue, -10)};
-      filter: darken(.5);
-    }
-  `};
+  ${(props) =>
+    props.onClick === null
+      ? null
+      : css`
+          cursor: pointer;
+          transition-duration: 0.25s;
+          &:hover {
+            // background: ${LightenDarkenColor(theme.colors.blue, -10)};
+            filter: darken(0.5);
+          }
+        `};
 
   ${Icon} {
     float: right;
   }
-`
+`;
 
-const toSpecialTime = hhmmap => {
+const toSpecialTime = (hhmmap) => {
   // Needs to be in "9:15 am" format
-  let hours = parseInt(hhmmap.split(':')[0])
-  const minutes = parseInt(hhmmap.split(':')[1].split(' ')[0])
-  const apm = hhmmap.split(' ')[1]
-  if (apm == 'pm' && hours != 12) {
-    hours += 12
+  let hours = parseInt(hhmmap.split(":")[0]);
+  const minutes = parseInt(hhmmap.split(":")[1].split(" ")[0]);
+  const apm = hhmmap.split(" ")[1];
+  if (apm == "pm" && hours != 12) {
+    hours += 12;
   }
 
-  let result = 0
-  result += hours * 60
-  result += minutes
+  let result = 0;
+  result += hours * 60;
+  result += minutes;
 
-  result -= 9 * 60 // cal starts at 9 am
-  result = result / 30 // each "tick" in the calendar is 30 minutes
+  result -= 7 * 60; // cal starts at 9 am
+  result = result / 30; // each "tick" in the calendar is 30 minutes
 
-  return result
-}
+  return result;
+};
 
 const components = {
-  p: Text
-}
+  p: Text,
+};
 
 class EventCard extends Component {
-  state = { active: false }
+  state = { active: false };
 
   toggle = () => {
-    this.setState(state => ({ active: !state.active }))
-  }
+    this.setState((state) => ({ active: !state.active }));
+  };
 
   render() {
-    const { active } = this.state
-    const { name, subtitle, start, end, summary, flavor } = this.props.event
+    const { active } = this.state;
+    const { name, subtitle, start, end, summary, flavor } = this.props.event;
     return (
       <>
         {active && (
@@ -109,15 +112,17 @@ class EventCard extends Component {
           start={start}
           end={end}
           flavor={flavor}
-          onClick={typeof summary === 'undefined' ? null : this.toggle}
+          onClick={typeof summary === "undefined" ? null : this.toggle}
         >
-          {typeof summary !== 'undefined' && <Icon glyph="external" size={24} />}
+          {typeof summary !== "undefined" && (
+            <Icon glyph="external" size={24} />
+          )}
           <Text.span bold>{name}</Text.span>
           <Text fontSize={1}>{subtitle}</Text>
         </Block>
       </>
-    )
+    );
   }
 }
 
-export default EventCard
+export default EventCard;
